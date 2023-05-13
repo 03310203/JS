4ff7b3826f03d0bc027eda9dcd461238edae3966
03310203 <118109551+03310203@users.noreply.github.com>
2023-05-13 10:55:57 +0900

    JS9の課題

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
-let lists = document.getElementsByClassName("pull-down-list")
-console.log(lists)
+window.addEventListener('load', function(){
+  
+const pullDownButton = document.getElementById("lists")
+
+
+pullDownButton.addEventListener('mouseover', function(){
+
+    console.log('乗せた');
+
+})
+
+pullDownButton.addEventListener('click', function(){
+
+    console.log('押した');
+
+})
+
+pullDownButton.addEventListener('mouseout', function(){
+
+    console.log('外した');
+})
+
+
+
+
+
+})
+
+
+
+
+
